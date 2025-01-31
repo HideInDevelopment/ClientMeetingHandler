bb097894e88cb96dedf8137e348e0374540a02a1
HideInDevelopment <hideindevelopment96@gmail.com>
2025-01-31 21:26:12 +0100

    main - Localization.cs creation

---
+++ BACK/ClientMeetingHandler/ClientMeetingHandler/domain/entities/Localization.cs
@@ -0,0 +1,17 @@
+namespace ClientMeetingHandler.domain.entities;
+
+internal class Localization
+{
+    public Guid Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+
+    public Localization(Guid id, string country, string city, string street)
+    {
+        Id = id;
+        Country = country;
+        City = city;
+        Street = street;
+    }
+}
